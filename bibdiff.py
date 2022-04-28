5ea4dc712e898c2831971bb3c5cbab680445ebd1
Jason M. Pittman <jason@jasonmpittman>
2022-04-28 09:28:19 -0400

    finalized logic

--- bibdiff.py
+++ bibdiff.py
@@ -7,26 +7,27 @@ import sys
 sys.path.append('biblib')
 import biblib.bib
 
+def main():
+    parser = argparse.ArgumentParser(description='Outputs a set of unique citation keys from two or more bibtext files')
 
-parser = argparse.ArgumentParser(description='Compute the symmetrical difference of two or more bibtext files')
+    parser.add_argument('files', nargs='+', help='list of bib files', type=open)
 
-parser.add_argument('files', nargs='+', help='list of bib files', type=open)
-parser.add_argument('output', type=str, help='the output file')
+    args = parser.parse_args()
 
-args = parser.parse_args()
+    try:
+        citeKeys = set()
+        
+        for f in args.files:
+            bib = biblib.bib.Parser().parse(f, log_fp=sys.stderr).get_entries()
+            for k in bib.keys():
+                citeKeys.add(k)
+        
+        print(citeKeys)             
 
-try:
-    citeKeys = set()
-    for f in args.files:
-        db = biblib.bib.Parser().parse(f, log_fp=sys.stderr).get_entries()
-        for k in db.keys():
-            citeKeys.add(k)
+    except Exception as e:
+        print(str(e))
 
-    print(citeKeys)
 
-except Exception as e:
-    print(str(e))
 
-def write_diff(output, bib):
-    pass
-
+if __name__ == "__main__":
+    main()
